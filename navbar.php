72e3f97863f916ab006087673f6ef65635171ed2
ZakyPutraKresya <zakyputrakresya@gmail.com>
2021-04-01 12:07:12 +0700

    update

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <div class="nav-bar bg-light shadow-sm fixed-top ">
      <div class="container">
        <nav class="navbar navbar-expand-lg navbar-light ">
-        <a class="navbar-brand" href="#">arshad</a><span class="span"></span>
+        <a class="navbar-brand" href="#">Zaky Putra</a><span class="span"></span>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
